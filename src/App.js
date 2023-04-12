a6095c14b04a88b8663874a01fe3ef83ff6af603
duongthaidat175@gmail.com <duongthaidat175@gmail.com>
2023-04-12 10:17:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+// import "./styles.css";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+const LoginPage = () => {
+    return ( 
+        <div>This is Login page</div>
+     );
+}
+ 
+export default LoginPage;
